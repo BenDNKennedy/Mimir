bc0cf211819467ecc5536dfc8ad2c7d152467af1
Ben Kennedy <72582729+BenDNKennedy@users.noreply.github.com>
2021-10-31 20:37:34 -0700

    Add files via upload

---
+++ mim.py
@@ -0,0 +1,68 @@
+import time, os, glob, re
+import discord
+import asyncio
+import webbrowser
+import keyboard
+
+
+client = discord.Client()
+running = False
+
+
+# Log when bot has come online
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    # Search for Chegg links in messages
+    if "https://www.chegg.com/homework-help/" in message.content:
+        # Check if bot is already chegging
+        global running
+        if running:
+            await message.reply("Please resend the link to get a response")
+            return
+
+        await message.add_reaction('\U0001F504')
+        running = True
+        # Get all Chegg homework URLs in message
+        url_list = re.findall(r'(https://(?:www.)?chegg.com/homework-help/\S+)', message.content)
+        
+        # Send images for every url
+        print(f' Chegging {url_list}')
+        for url in url_list:
+            # Open Chegg link
+            webbrowser.open(url)
+            time.sleep(5)
+
+            # Take a screenshot using the extension
+            keyboard.press_and_release('alt+shift+p')
+            # Textbook pages take much longer to capture
+            if "/homework-help/questions-and-answers/" in url:
+                time.sleep(7)
+            else:
+                time.sleep(10)
+
+            # Close the Chegg window
+            keyboard.press_and_release('ctrl+w')
+
+            # Get the file of the image by finding the newest one
+            path = "C:/Users/[YOUR USERNAME]/Downloads/screenshots/*"
+            file_loc = max(glob.glob(path, recursive=True), key=os.path.getmtime)
+
+            # Send the image
+            file = [discord.File(file_loc)]
+            await message.channel.send(files=file)
+
+            # Delete the image
+            os.remove(file_loc)
+        await message.add_reaction('\U00002705')
+        running = False
+
+
+# Run the bot
+with open('key.txt') as f:
+    key = f.read()
+client.run(key)
